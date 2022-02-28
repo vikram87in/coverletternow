db5d967a90d2e4e9c1dec23714b8f4432dbd76f5
vikram singh <vikram.singh@bold.com>
2022-02-28 15:14:53 +0530

    basic set up for CLN POC: site working with build-letter url

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Node: Cover Letter Now' });
+})
+
+app.listen(PORT, () => console.log(`Express server started at http://localhost:${PORT}`));      
